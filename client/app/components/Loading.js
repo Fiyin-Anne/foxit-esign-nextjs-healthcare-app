0f1c0a16f46fada94836b296285c088f4455c17e
fiyin-anne <fiyinakinsiku@gmail.com>
2023-10-16 22:42:49 +0100

    change directory

--- client/app/components/Loading.js
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-    return (
-      <div className="h-[calc(100vh-60px)] bg-gray-200 w-full grid place-items-center">
-        <span className="loading loading-spinner loading-lg"></span>
-      </div>
-    );
-};
-
-export default Loading;
